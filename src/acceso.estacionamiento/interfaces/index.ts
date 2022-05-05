9b23b6c62b2a66bff3932ab9ce504fd523c6dde0
joackob <89130929+jblancoetec@users.noreply.github.com>
2022-05-05 01:43:58 -0300

    create basic project for exercises

--- src/acceso.estacionamiento/interfaces/index.ts
+++ src/acceso.estacionamiento/interfaces/index.ts
@@ -1,21 +1,24 @@
 export abstract class ILectorDeTarjeta {
-  constructor(protected readonly seguridad: ISeguridadEstacionamiento) {}
+  constructor(protected readonly estacionamiento: IEstacionamiento) {}
   abstract recibirTarjeta(): void;
   abstract devolverTarjeta(): void;
   abstract mostrarMensajeError(): void;
   abstract mostrarMensajeExito(): void;
 }
 
-export abstract class ISeguridadEstacionamiento {
+export abstract class IEstacionamiento {
   constructor(
     protected readonly lector: ILectorDeTarjeta,
-    protected readonly registro: IRegistroDeAcceso,
+    protected readonly acceso: IAcceso,
     protected readonly barrera: IBarrera
   ) {}
-  abstract verificarCodigoTarjeta(codigoTarjeta: string): void;
+  abstract verificarCodigoTarjeta(
+    codigoTarjeta: string,
+    lectorConTarjeta: ILectorDeTarjeta
+  ): void;
 }
 
-export interface IRegistroDeAcceso {
+export interface IAcceso {
   grabarUltimoAcceso(codigoTarjeta: string): void;
 }
 
--- src/app/App.ts
+++
@@ -1,9 +0,0 @@
-import { ILectorDeTarjeta } from "../interfaces";
-
-class App {
-  constructor(private readonly lector: ILectorDeTarjeta) {}
-
-  init() {
-    this.lector.recibirTarjeta();
-  }
-}
