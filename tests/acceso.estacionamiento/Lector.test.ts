5ae2ee857a2b3541129ebb9c5c084987b031aa6d
Joaquin Blanco <jblanco@etec.uba.ar>
2022-05-13 06:01:17 -0300

    update test

--- tests/acceso.estacionamiento/Lector.test.ts
+++ tests/acceso.estacionamiento/Lector.test.ts
@@ -1,23 +1,11 @@
 import { Lector, Barrera, Acceso } from "../../src/acceso.estacionamiento";
 import Estacionamiento from "../../src/acceso.estacionamiento/Estacionamiento";
-jest.mock("../../src/acceso.estacionamiento/Estacionamiento"); //now Estacionamiento is a mock constructor
+jest.mock("../../src/acceso.estacionamiento/Estacionamiento");
 
-import {
-  IEstacionamiento,
-  ILectorDeTarjeta,
-} from "../../src/acceso.estacionamiento";
-
-const estacionamiento: IEstacionamiento = new Estacionamiento(
-  new Acceso(),
-  new Barrera()
-);
-const lector: ILectorDeTarjeta = new Lector(estacionamiento);
-
-beforeEach(() => {
-  jest.clearAllMocks();
-  lector.recibirTarjeta();
-});
+const estacionamiento = new Estacionamiento(new Acceso(), new Barrera());
+const lector = new Lector(estacionamiento);
 
 test("lector debe recibir la tarjeta y verificar codigo con el estacionamiento", () => {
+  lector.recibirTarjeta();
   expect(estacionamiento.verificarCodigoTarjeta).toBeCalledWith("12", lector);
 });
