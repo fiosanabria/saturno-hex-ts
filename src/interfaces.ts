e7c98cd3fe472cad3940b5526ad67df52a0736f8
joackob <89130929+jblancoetec@users.noreply.github.com>
2022-05-24 11:48:56 -0300

    Create interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,26 @@
+export abstract class ILectorDeTarjeta {
+  constructor(protected readonly estacionamiento: IEstacionamiento) {}
+  abstract recibirTarjeta(): void;
+  abstract devolverTarjeta(): void;
+  abstract mostrarMensajeError(): void;
+  abstract mostrarMensajeExito(): void;
+}
+
+export abstract class IEstacionamiento {
+  constructor(
+    protected readonly acceso: IAcceso,
+    protected readonly barrera: IBarrera
+  ) {}
+  abstract verificarCodigoTarjeta(
+    codigo: string,
+    lector: ILectorDeTarjeta
+  ): void;
+}
+
+export interface IAcceso {
+  grabarUltimoAcceso(codigoTarjeta: string): void;
+}
+
+export interface IBarrera {
+  elevar(): void;
+}
