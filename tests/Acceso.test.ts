4ecbac9125ecc179d66910b0d3a663fc40253f53
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2022-05-24 17:23:45 -0300

    Update Acceso.test.ts

--- tests/Acceso.test.ts
+++ tests/Acceso.test.ts
@@ -6,3 +6,7 @@ const acceso: IAcceso = new Acceso();
 test("Deberia existir una clase Acceso que implemente IAcceso", () => {
   expect(acceso).toBeDefined();
 });
+
+test("Deberia ejecutarse el metodo guardarUltimoAcceso sin problemas", () => {
+  acceso.grabarUltimoAcceso("123456789");
+});
