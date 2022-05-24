7e5a2abc2e50c55ecc6d99ca9fcf4c3f29cd306b
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2022-05-24 18:17:28 -0300

    Update Lector.test.ts

--- tests/Lector.test.ts
+++ tests/Lector.test.ts
@@ -8,3 +8,24 @@ const lector: ILectorDeTarjeta = new Lector(estacionamiento);
 test("Deberia existir una clase Lector que implemente ILectorDeTarjeta", () => {
   expect(lector).toBeDefined();
 });
+
+test("Deberia ejecutarse el metodo recibirTarjeta sin problemas", () => {
+  expect(() => lector.recibirTarjeta()).not.toThrow();
+});
+
+test("Tras recibir la tarjeta, el lector debe verificar el codigo de la tarjeta con el estacionamiento", () => {
+  lector.recibirTarjeta();
+  expect(estacionamiento.verificarCodigoTarjeta).toBeCalledWith("123", lector);
+});
+
+test("Deberia ejecutarse el metodo devolverTarjeta sin problemas", () => {
+  expect(() => lector.devolverTarjeta()).not.toThrow();
+});
+
+test("Deberia ejecutarse el metodo mostrarMensajeError sin problemas", () => {
+  expect(() => lector.mostrarMensajeError()).not.toThrow();
+});
+
+test("Deberia ejecutarse el metodo mostrarMensajeExito sin problemas", () => {
+  expect(() => lector.mostrarMensajeExito()).not.toThrow();
+});
