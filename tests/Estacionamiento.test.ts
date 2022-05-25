cf8b9d0ea0622fcabb1bd8375f2279b28ce22b62
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2022-05-25 13:01:04 -0300

    Update Estacionamiento.test.ts

--- tests/Estacionamiento.test.ts
+++ tests/Estacionamiento.test.ts
@@ -19,3 +19,23 @@ test("Deberia ejecutarse el metodo verificarCodigoTarjeta sin problemas", () => 
     estacionamiento.verificarCodigoTarjeta("123", lector)
   ).not.toThrow();
 });
+
+test("Si el codigo a verificar es 123, este debe ser aceptado y se debe grabar el ultimo acceso", () => {
+  estacionamiento.verificarCodigoTarjeta("123", lector);
+  expect(acceso.grabarUltimoAcceso).toBeCalledWith("123");
+});
+
+test("Si el codigo a verificar es 456, este debe ser aceptado y se debe grabar el ultimo acceso", () => {
+  estacionamiento.verificarCodigoTarjeta("456", lector);
+  expect(acceso.grabarUltimoAcceso).toBeCalledWith("456");
+});
+
+test("Si el codigo a verificar es 456, este debe ser aceptado y se debe elevar la barrera", () => {
+  estacionamiento.verificarCodigoTarjeta("456", lector);
+  expect(barrera.elevar).toBeCalled();
+});
+
+test("Si el codigo a verificar es 456, este debe ser aceptado y se debe enviar un mensaje de exito al lector", () => {
+  estacionamiento.verificarCodigoTarjeta("456", lector);
+  expect(lector.mostrarMensajeExito).toBeCalled();
+});
